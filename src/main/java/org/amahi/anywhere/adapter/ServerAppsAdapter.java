b714dd3cc37ba58f36c83058aecbaf805623de92
Artur Dryomov <artur.dryomov@gmail.com>
2014-07-17 11:51:03 +0300

    Add apps basic UI support.

---
+++ src/main/java/org/amahi/anywhere/adapter/ServerAppsAdapter.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2014 Amahi
+ *
+ * This file is part of Amahi.
+ *
+ * Amahi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Amahi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Amahi. If not, see <http ://www.gnu.org/licenses/>.
+ */
+
+package org.amahi.anywhere.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.amahi.anywhere.R;
+import org.amahi.anywhere.server.model.ServerApp;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ServerAppsAdapter extends BaseAdapter
+{
+	private final LayoutInflater layoutInflater;
+
+	private List<ServerApp> apps;
+
+	public ServerAppsAdapter(Context context) {
+		this.layoutInflater = LayoutInflater.from(context);
+
+		this.apps = Collections.emptyList();
+	}
+
+	public void replaceWith(List<ServerApp> apps) {
+		this.apps = apps;
+
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return apps.size();
+	}
+
+	public List<ServerApp> getItems() {
+		return apps;
+	}
+
+	@Override
+	public ServerApp getItem(int position) {
+		return apps.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup container) {
+		ServerApp app = getItem(position);
+
+		if (view == null) {
+			view = newView(container);
+		}
+
+		bindView(app, view);
+
+		return view;
+	}
+
+	private View newView(ViewGroup container) {
+		return layoutInflater.inflate(R.layout.view_server_app_item, container, false);
+	}
+
+	private void bindView(ServerApp app, View view) {
+		TextView appView = (TextView) view;
+
+		appView.setText(getAppName(app));
+	}
+
+	private String getAppName(ServerApp app) {
+		return app.getName();
+	}
+}

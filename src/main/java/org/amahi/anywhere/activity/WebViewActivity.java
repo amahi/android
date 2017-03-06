1a2969c07c45b59fab9b4544eed80986150abbf3
Sukhbir Singh <sukhbir947@gmail.com>
2017-03-07 04:39:51 +0530

    Update WebActivity.java

--- src/main/java/org/amahi/anywhere/activity/WebViewActivity.java
+++ src/main/java/org/amahi/anywhere/activity/WebViewActivity.java
@@ -1,7 +1,6 @@
 package org.amahi.anywhere.activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;

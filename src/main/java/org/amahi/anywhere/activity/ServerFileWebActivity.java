fed67e4e109a0f8a5f7fbe7102800d2e5709cc07
Kumar Shashwat <allblue.shasha@gmail.com>
2017-04-22 12:21:09 +0530

    Replace webviews with custom tabs, so that htpassword and other things work better (#193)
    fixes #171.
    
    however, i think #192 is still open.
    
    * Removed the WebView from the activity_server_file_web.

--- src/main/java/org/amahi/anywhere/activity/ServerFileWebActivity.java
+++ src/main/java/org/amahi/anywhere/activity/ServerFileWebActivity.java
@@ -19,9 +19,15 @@
 
 package org.amahi.anywhere.activity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.webkit.WebView;
@@ -48,6 +54,10 @@ import javax.inject.Inject;
 public class ServerFileWebActivity extends AppCompatActivity
 {
 	private static final Set<String> SUPPORTED_FORMATS;
+	CustomTabsClient mCustomTabsClient;
+	CustomTabsSession mCustomTabsSession;
+	CustomTabsServiceConnection mCustomTabsServiceConnection;
+	CustomTabsIntent mCustomTabsIntent;
 
 	static {
 		SUPPORTED_FORMATS = new HashSet<String>(Arrays.asList(
@@ -67,8 +77,6 @@ public class ServerFileWebActivity extends AppCompatActivity
 
 		setUpInjections();
 
-		setUpHomeNavigation();
-
 		setUpWebResource(savedInstanceState);
 	}
 
@@ -76,40 +84,43 @@ public class ServerFileWebActivity extends AppCompatActivity
 		AmahiApplication.from(this).inject(this);
 	}
 
-	private void setUpHomeNavigation() {
-		getSupportActionBar().setHomeButtonEnabled(true);
-		getSupportActionBar().setIcon(R.drawable.ic_launcher);
-	}
-
 	private void setUpWebResource(Bundle state) {
-		setUpWebResourceTitle();
-		setUpWebResourceClient();
 		setUpWebResourceContent(state);
 	}
 
-	private void setUpWebResourceTitle() {
-		getSupportActionBar().setTitle(getFile().getName());
-	}
-
-	private void setUpWebResourceClient() {
-		getWebView().setWebViewClient(new WebResourceClient(this));
-	}
-
 	private void setUpWebResourceContent(Bundle state) {
-		if (!isWebResourceStateValid(state)) {
-			getWebView().getSettings().setLoadWithOverviewMode(true);
-			getWebView().getSettings().setUseWideViewPort(true);
-			getWebView().getSettings().setBuiltInZoomControls(true);
-			getWebView().loadUrl(getWebResourceUri().toString());
+		if (!isWebResourceStateValid(state)){
+			setUpCustomTabs();
 		}
+	}
+
+	private void setUpCustomTabs(){
+
+		mCustomTabsServiceConnection = new CustomTabsServiceConnection() {
+			@Override
+			public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
+				mCustomTabsClient= customTabsClient;
+				mCustomTabsClient.warmup(0L);
+				mCustomTabsSession = mCustomTabsClient.newSession(null);
+			}
+
+			@Override
+			public void onServiceDisconnected(ComponentName name) {
+				mCustomTabsClient= null;
+			}
+		};
+
+		CustomTabsClient.bindCustomTabsService(this, getPackageName(), mCustomTabsServiceConnection);
+
+		mCustomTabsIntent = new CustomTabsIntent.Builder(mCustomTabsSession)
+				.setShowTitle(true)
+				.build();
+
+		mCustomTabsIntent.launchUrl(this,getWebResourceUri());
 	}
 
 	private boolean isWebResourceStateValid(Bundle state) {
 		return state != null;
-	}
-
-	private WebView getWebView() {
-		return (WebView) findViewById(R.id.web_content);
 	}
 
 	private Uri getWebResourceUri() {
@@ -124,72 +135,13 @@ public class ServerFileWebActivity extends AppCompatActivity
 		return getIntent().getParcelableExtra(Intents.Extras.SERVER_FILE);
 	}
 
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		super.onRestoreInstanceState(savedInstanceState);
-
-		setUpWebResourceState(savedInstanceState);
-	}
-
-	private void setUpWebResourceState(Bundle state) {
-		getWebView().restoreState(state);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem menuItem) {
-		switch (menuItem.getItemId()) {
-			case android.R.id.home:
-				finish();
-				return true;
-
-			default:
-				return super.onOptionsItemSelected(menuItem);
-		}
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-
-		tearDownWebResourceState(outState);
-	}
-
-	private void tearDownWebResourceState(Bundle state) {
-		getWebView().saveState(state);
-	}
-
 	public static boolean supports(String mime_type) {
 		return SUPPORTED_FORMATS.contains(mime_type);
 	}
 
-	private static final class WebResourceClient extends WebViewClient
-	{
-		private final ServerFileWebActivity activity;
-
-		public WebResourceClient(ServerFileWebActivity activity) {
-			this.activity = activity;
-		}
-
-		@Override
-		public void onPageStarted(WebView appWebView, String appUrl, Bitmap appFavicon) {
-			super.onPageStarted(appWebView, appUrl, appFavicon);
-
-			activity.showProgress();
-		}
-
-		@Override
-		public void onPageFinished(WebView appWebView, String appUrl) {
-			super.onPageFinished(appWebView, appUrl);
-
-			activity.showApp();
-		}
-	}
-
-	private void showProgress() {
-		ViewDirector.of(this, R.id.animator).show(android.R.id.progress);
-	}
-
-	private void showApp() {
-		ViewDirector.of(this, R.id.animator).show(R.id.web_content);
+	@Override
+	protected void onResume() {
+		super.onResume();
+		onBackPressed();
 	}
 }

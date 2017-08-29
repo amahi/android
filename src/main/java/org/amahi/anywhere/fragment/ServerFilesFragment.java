37c8ec2cc484f83636627e8b8cdd9800fe1803b3
Chirag Maheshwari <csoni111@gmail.com>
2017-08-29 09:01:06 +0530

    Adds Chromecast functionality (#273)
    * Chromecast support (#266)
    
    * resolve TransactionTooLargeException on android 7
    
    * adds chromecast support
    
    * adds cast session manager and remote media to native player
    
    * adds ExpandedController for cast
    
    * resolve blank video activity on stopping cast
    
    * adds mini controller
    
    * adds cast notifications and change app id
    
    * resolve crash on opening cast activity through notification
    
    * adds cast support for audio files
    
    * start casting only after metadata is fetched
    
    * adds fixed image audio cast
    
    * shift chromecast app_id in api.properties
    
    * remove custom audio-play image
    
    * adds cast options to image viewer (#269)

--- src/main/java/org/amahi/anywhere/fragment/ServerFilesFragment.java
+++ src/main/java/org/amahi/anywhere/fragment/ServerFilesFragment.java
@@ -27,6 +27,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Parcelable;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
@@ -48,6 +49,11 @@ import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastState;
+import com.google.android.gms.cast.framework.CastStateListener;
+import com.google.android.gms.cast.framework.IntroductoryOverlay;
 import com.squareup.otto.Subscribe;
 
 import org.amahi.anywhere.AmahiApplication;
@@ -81,16 +87,21 @@ import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 /**
  * Files fragment. Shows files list.
  */
-public class ServerFilesFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener,
+public class ServerFilesFragment extends Fragment implements
+	SwipeRefreshLayout.OnRefreshListener,
 	AdapterView.OnItemClickListener,
 	AdapterView.OnItemLongClickListener,
 	ActionMode.Callback,
 	SearchView.OnQueryTextListener,
-	FilesFilterBaseAdapter.onFilterListChange
+	FilesFilterBaseAdapter.onFilterListChange,
+	CastStateListener
 {
 	private SearchView searchView;
 	private MenuItem searchMenuItem;
 	private LinearLayout mErrorLinearLayout;
+	private CastContext mCastContext;
+	private IntroductoryOverlay mIntroductoryOverlay;
+	private MenuItem mediaRouteMenuItem;
 
 	private static final class State
 	{
@@ -133,11 +144,51 @@ public class ServerFilesFragment extends Fragment implements SwipeRefreshLayout.
 
 		setUpInjections();
 
+		setUpCast();
+
 		setUpFiles(savedInstanceState);
 	}
 
 	private void setUpInjections() {
 		AmahiApplication.from(getActivity()).inject(this);
+	}
+
+	private void setUpCast() {
+		mCastContext = CastContext.getSharedInstance(getActivity());
+	}
+
+
+	@Override
+	public void onCastStateChanged(int newState) {
+		if (newState != CastState.NO_DEVICES_AVAILABLE) {
+			showIntroductoryOverlay();
+		}
+	}
+
+	private void showIntroductoryOverlay() {
+		if (mIntroductoryOverlay != null) {
+			mIntroductoryOverlay.remove();
+		}
+		if ((mediaRouteMenuItem != null) && mediaRouteMenuItem.isVisible()) {
+			new Handler().post(new Runnable() {
+				@Override
+				public void run() {
+					mIntroductoryOverlay = new IntroductoryOverlay
+							.Builder(getActivity(), mediaRouteMenuItem)
+							.setTitleText("Introducing Cast")
+							.setSingleTime()
+							.setOnOverlayDismissedListener(
+									new IntroductoryOverlay.OnOverlayDismissedListener() {
+										@Override
+										public void onOverlayDismissed() {
+											mIntroductoryOverlay = null;
+										}
+									})
+							.build();
+					mIntroductoryOverlay.show();
+				}
+			});
+		}
 	}
 
 	private void setUpFiles(Bundle state) {
@@ -511,6 +562,10 @@ public class ServerFilesFragment extends Fragment implements SwipeRefreshLayout.
 		super.onCreateOptionsMenu(menu, menuInflater);
 
 		menuInflater.inflate(R.menu.action_bar_server_files, menu);
+
+		mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(
+				getActivity().getApplicationContext(),
+				menu, R.id.media_route_menu_item);
 	}
 
 	@Override
@@ -628,6 +683,7 @@ public class ServerFilesFragment extends Fragment implements SwipeRefreshLayout.
 	public void onResume() {
 		super.onResume();
 
+		mCastContext.addCastStateListener(this);
 		BusProvider.getBus().register(this);
 	}
 
@@ -635,6 +691,7 @@ public class ServerFilesFragment extends Fragment implements SwipeRefreshLayout.
 	public void onPause() {
 		super.onPause();
 
+		mCastContext.removeCastStateListener(this);
 		BusProvider.getBus().unregister(this);
 	}
 

6fc9c8a7a587adcf6cd562d1d4ccbd1fccbaf128
mohitsvnit <mohitsvnit093@gmail.com>
2018-03-04 22:56:09 +0530

    Used android inbuilt selectableItemBackground for pressing effect

--- src/main/java/org/amahi/anywhere/adapter/ServerSharesAdapter.java
+++ src/main/java/org/amahi/anywhere/adapter/ServerSharesAdapter.java
@@ -20,13 +20,10 @@
 package org.amahi.anywhere.adapter;
 
 import android.content.Context;
-import android.os.Build;
 import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.amahi.anywhere.R;

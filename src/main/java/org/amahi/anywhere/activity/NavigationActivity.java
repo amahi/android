698a68151bef064dd905ed6ff274a90d2361ce23
Aditya Arora <aadityaaaaroraa@gmail.com>
2019-03-20 13:00:10 +0530

    Fix Android TV app stuck after Intro
    This prevents the app from looping into Intro
    Activity and fixes the issue https://github.com/amahi/android/issues/422

--- src/main/java/org/amahi/anywhere/activity/NavigationActivity.java
+++ src/main/java/org/amahi/anywhere/activity/NavigationActivity.java
@@ -138,7 +138,7 @@ public class NavigationActivity extends AppCompatActivity implements DrawerLayou
     }
 
     private void hideActionBar() {
-        getSupportActionBar().hide();
+        if (getSupportActionBar()!=null) getSupportActionBar().hide();
     }
 
     private void setUpInjections() {

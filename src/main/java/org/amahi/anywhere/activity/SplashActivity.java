e738ec189ace6b3eae57e85108396c8e81f0f464
prince-09 <princesinha7072@gmail.com>
2020-03-20 18:47:28 +0530

    should be using handler in splash screen

--- src/main/java/org/amahi/anywhere/activity/SplashActivity.java
+++ src/main/java/org/amahi/anywhere/activity/SplashActivity.java
@@ -21,6 +21,8 @@ package org.amahi.anywhere.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,8 +31,14 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setUpActivity();
+            }
+        },2000);
 
-        setUpActivity();
 
         finish();
     }

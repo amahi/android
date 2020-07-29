f252553db043877d0e4d3ca53967a3d0fbe7d77f
Chirag Jain <jainchg@gmail.com>
2020-07-29 16:39:27 +0530

    Add Custom Tabs to WebView (#573)
    * Add Custom Tabs to WebView
    * Add License and theme to CustomTabs

--- src/main/java/org/amahi/anywhere/activity/ServerFileWebActivity.java
+++ src/main/java/org/amahi/anywhere/activity/ServerFileWebActivity.java
@@ -117,6 +117,7 @@ public class ServerFileWebActivity extends AppCompatActivity {
         CustomTabsClient.bindCustomTabsService(this, getPackageName(), mCustomTabsServiceConnection);
 
         mCustomTabsIntent = new CustomTabsIntent.Builder(mCustomTabsSession)
+            .setToolbarColor(getResources().getColor(R.color.primary))
             .setShowTitle(true)
             .build();
 

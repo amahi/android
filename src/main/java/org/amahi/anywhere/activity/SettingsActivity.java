4283dafa48daaab895c9939ad83efde95c2b9807
Prakhar Agarwal <prakhar@everything.net.in>
2020-11-04 12:55:28 +0530

    Replaced switch with if/else statements for Non-Constant Fields in Case Labels

--- src/main/java/org/amahi/anywhere/activity/SettingsActivity.java
+++ src/main/java/org/amahi/anywhere/activity/SettingsActivity.java
@@ -82,14 +82,11 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(menuItem);
+        if (menuItem.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
+        return super.onOptionsItemSelected(menuItem);
     }
 
     @Subscribe
